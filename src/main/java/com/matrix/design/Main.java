f10807c52aa6c419c5c6a177d5f6a03c206e2ebf
Jisang Lee <matrixpower1004@gmail.com>
2024-02-10 03:03:05 +0900

    Init: gradle 의존성 추가 (project 구조 변경)

--- src/main/java/com/matrix/design/Main.java
+++ src/main/java/com/matrix/design/Main.java
@@ -1,7 +1,8 @@
 package com.matrix.design;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello world!");
     }
 }
